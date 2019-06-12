037b5155919d1d719c219617cc21282415137538
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-06-12 16:43:36 -0600

    pausing

--- assets/javascript/gif_app.js
+++ assets/javascript/gif_app.js
@@ -5,6 +5,7 @@ var instruments = ["oboe", "piano", "bassoon", "piccolo", "flute", "cymbal", "ty
 
 
 function display_buttons() {
+    $("#instrument_buttons").empty();
     instruments.forEach(function (item) {
         var $button = $("<button>").attr("data-instrument", item);
         $button.text(item);
@@ -12,7 +13,23 @@ function display_buttons() {
     });
 }
 
-display_buttons();
+
+  //  On Click event associated with the add-to-do function
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
+
+    var $new_instrument = $("#instrument_text").val().trim();
+    instruments.push($new_instrument);
+
+    // console.log($new_instrument);
+    $("#instrument_text").val("");
+
+    display_buttons();
+    
+  });
+
+
+
 
 $("#instrument_buttons").on("click", "button", function () {
 
@@ -26,14 +43,15 @@ $("#instrument_buttons").on("click", "button", function () {
         $instrument + "&api_key=rlRqjccJB8HqG3sPhUDloKcGMFiVcLPd&limit=3";
 
     $.get({ url: queryURL, }).then(function (response) {
-        console.log(queryURL);
-        console.log(response);
         var results = response.data;
         for (var i = 0; i < results.length; i++) {
             var instrument_div = $("<div>");
             var p = $("<p>").text("Rating: " + results[i].rating);
+
             var instrument_image = $("<img>");
             instrument_image.attr("src", results[i].images.fixed_height.url);
+
+
             instrument_div.append(p);
             instrument_div.append(instrument_image);
 
@@ -41,3 +59,30 @@ $("#instrument_buttons").on("click", "button", function () {
         }
     });
 });
+
+
+//     <img src="https://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif"
+// \ data-still="https://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif" 
+// data-animate="https://media3.giphy.com/media/W6LbnBigDe4ZG/200.gif" 
+// data-state="still" 
+// class="gif">
+
+    
+
+  $("#gif_display").on("click","img", function() {
+      alert("image clicked");
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    // var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    // if (state === "still") {
+    //   $(this).attr("src", $(this).attr("data-animate"));
+    //   $(this).attr("data-state", "animate");
+    // } else {
+    //   $(this).attr("src", $(this).attr("data-still"));
+    //   $(this).attr("data-state", "still");
+    // }
+  });
+
+display_buttons();
